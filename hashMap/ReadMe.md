1688b67b55045e8034978abebb25ae280848045e
dholness <dholness@thoughtworks.com>
2018-01-17 20:55:57 -0500

    intial commit

---
+++ hashMap/ReadMe.md
@@ -0,0 +1,37 @@
+# Problem
+Implement a hashmap (with amortized constant time look-ups) in Python without using a hashmap primitive.
+Please include an executable testing framework for your data structure.
+
+#Approach
+  To achieve a hashmap implementation with amortized constant time look-ups, this project relys on Python's list data
+ structure to store a fixed amount of buckets that will contain link-lists. The link-lists contain nodes containg keys
+ and associated values. In situations of hash collisions a new entry node is added to the link list.The base list  contiaing the buckets  of
+ link lists is dynamic and will double in size every time the number of hashmap entries / list size rises to a ratio of 1. As a result
+ we have a amortized constant time look-ups.
+
+##Requirments
+ 1. Python 3.6.2
+ 2. pytest version = '3.3.2'
+ 3. Mac OS X
+
+## Setup
+
+ 1. To install Homebrew, open Terminal
+  `$ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"`
+
+ 2.
+
+ 3.`$ brew install python3.3.2`
+
+ 4. `pip install pytest`
+
+## To Runt Tests
+
+1. Move to Dir:
+ `$ cd hashmap`
+
+2. Run tests:
+ `$ pytest`
+
+
+
---
+++ hashMap/test_linkList.py
@@ -0,0 +1,43 @@
+from hashMap.linklist import LinkList
+from hashMap.node import Node
+
+
+class TestLinkList:
+
+    def test_add_node_list(self):
+        test_list = LinkList()
+        test_list.add_node("a", "apple")
+        test_list.add_node("z", "zebra")
+        assert test_list.first_node.key == "a"
+        assert test_list.last_node.key == "z"
+        assert test_list.first_node.value == "apple"
+        assert test_list.last_node.value == "zebra"
+
+    def test_append_new_node_to_list(self):
+        test_node = Node("d", 5)
+        test_list = LinkList()
+        test_list.append_new_node_to_list(test_node)
+
+    def test_contains_value(self):
+        test_list = LinkList()
+        test_list.add_node("a", "apple")
+        assert True == test_list.contains_value("apple")
+
+    def test_contains_key(self):
+        test_list = LinkList()
+        test_list.add_node("a", "aaa")
+        assert True == test_list.contains_key("a")
+
+    def test_delete_node_with_key(self):
+        test_list = LinkList()
+        test_list.add_node("z", "zebra")
+        test_list.delete_node_with_key("z")
+        assert False == test_list.contains_key("z")
+        assert False == test_list.contains_value("zebra")
+
+    def test_clear(self):
+        test_list = LinkList()
+        test_list.add_node("a", "apple")
+        test_list.clear()
+        assert test_list.first_node == None
+        assert test_list.last_node == None
